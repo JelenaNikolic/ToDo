beb9a726384d86d0d859744918055ad8fd75cdaa
Vladislav Mosnak <vmosnak@yahoo.com>
2016-03-03 19:02:00 +0100

    update

--- add_new.php
+++ add_new.php
@@ -1,7 +1,7 @@
 <?php
 
 include("mysqli_konekcija.php");
-
+error_reporting(E_ERROR);
 $text = $_POST['text'];
 
 $sql = $dbc->prepare("INSERT INTO todos (text) VALUES (?)");
@@ -10,10 +10,8 @@ $sql->execute();
 $sql->close();
 
 $response['id'] = mysql_insert_id();
+$response['error'] = false;
+$response['message'] = 'New To Do Added';
 
-if($response['id']){
-	$response['error'] = false;
-	$response['message'] = 'New To Do Added';
-}
 echo json_encode($response);
 ?>
